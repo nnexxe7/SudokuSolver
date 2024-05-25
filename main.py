b57a0e9aeac78567da2bee43d26f81469eaefe90
nnexxe <michal21x@gmail.com>
2024-05-25 23:37:34 +0200

    Add Backtracking Algorithm and test it with simple board

---
+++ main.py
@@ -0,0 +1,57 @@
+def is_valid(board, row, col, num):
+    for x in range(9):
+        if board[row][x] == num:
+            return False
+
+    for x in range(9):
+        if board[x][col] == num:
+            return False
+
+    start_row = row - row % 3
+    start_col = col - col % 3
+    for i in range(3):
+        for j in range(3):
+            if board[i + start_row][j + start_col] == num:
+                return False
+    return True
+
+def solve_sudoku(board):
+    empty = find_empty(board)
+    if not empty:
+        return True
+    else:
+        row, col = empty
+
+    for num in range(1, 10):
+        if is_valid(board, row, col, num):
+            board[row][col] = num
+            if solve_sudoku(board):
+                return True
+            board[row][col] = 0
+
+    return False
+
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)
+    return None
+
+board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+if solve_sudoku(board):
+    for row in board:
+        print(row)
+else:
+    print("No solution exists")
